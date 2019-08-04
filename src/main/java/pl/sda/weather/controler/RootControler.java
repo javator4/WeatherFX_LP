cbd1686be86a1bef557a6817cb01e8308e8437ea
LukaszPietrzak <pietrzakl90@gmail.com>
2019-08-04 10:09:10 +0200

    add pop up window

--- src/main/java/pl/sda/weather/controler/RootControler.java
+++ src/main/java/pl/sda/weather/controler/RootControler.java
@@ -2,12 +2,15 @@ package pl.sda.weather.controler;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
-public class RootControler {
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class RootControler implements Initializable {
     @FXML
     private TextField city;
 
@@ -22,5 +25,24 @@ public class RootControler {
         String text = city.getText();
         temperature.setText(text);
         System.out.println("klik");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("uruchomiłem okno");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Info");
+        alert.setContentText("To moja aplikacja");
+        alert.show();
+
+        Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
+        alert1.setTitle("Potwierdzenie");
+        alert1.setContentText("Jesteś pewien?");
+        Optional<ButtonType> result =alert1.showAndWait();
+        if(result.get() == ButtonType.OK){
+            System.out.println("Użytkownik wybrał ok");
+        }else {
+            System.out.println("Uzytkownik wybrał cancel");
+        }
     }
 }
